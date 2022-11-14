fc166e9e198c870cb3bdac4a83b9cd9d67361885
ARESKAN2108 <maxareskan@gmail.com>
2022-11-14 23:13:45 +0300

    fixed

--- tasks/IsItAPalindrome.js
+++ tasks/IsItAPalindrome.js
@@ -1,14 +1,13 @@
 // Write a function that checks if a given string (case insensitive) is a palindrome.
 function isPalindrome(x) {
     // your code here
-    x = x.toLowerCase();
+    x = x.toLowerCase().replace(/[^а-яa-z1-9]/gi, "");
     const lastIndex = x.length - 1;
     for (let i = 0; i < x.length / 2; i++) {
         if (x[i] !== x[lastIndex - i]) {
             return false;
-        } else
-        return true;
+        } 
     }
+    return true;
   }
-  isPalindrome("aavavavava");
-console.log(isPalindrome("aavavavava"));
+console.log(isPalindrome("aavvavavavav"));
